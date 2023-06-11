9922f0bc3d050255f6013d9f69aa6b94fbc42031
eldrich-prog <eldrich.marin@utp.edu.co>
2023-06-10 21:11:26 -0600

    splitting messages into two processes and two sockets

--- send.py
+++ send.py
@@ -2,19 +2,60 @@ from abc import ABC
 import socket
 import asyncio
 import multiprocessing
-import server
 
-class Send_Message(server.Connection):
+class Connection(ABC):
+
+    def __init__(self, port, buffer):
+        # Create a datagram socket
+        self.server = socket.socket(family = socket.AF_INET, type = socket.SOCK_DGRAM)
+        self.host = socket.gethostbyname(socket.gethostname())
+        self.port = port
+        self.buffer = buffer
+        # Bind to host and port
+        self.server.bind((self.host, self.port))
+        print("UDP server up and listening")
+
+    def encode(self, message):
+        return message.encode(encoding = 'UTF-8', errors = "strict")
+    
+    def decode(self, message):
+        return message.decode('UTF-8')
+class Contacts:
+
+    def __init__(self):
+        self.contacts = {
+            'eldrich': '192.168.50.10'
+        }
+        
+    def search_contact(self, name):
+        if name in self.contacts:
+            return self.contacts[name]
+        else: 
+            return False
+        
+    def add_contact(self, name, ip_address):
+        if self.search_contact(name):
+            print("ya existe")
+        else:
+            self.contacts[name] = ip_address
+            print(f'Contact: {name} - Address: {ip_address} Incorporated.')
+
+
+class Send_Message(Connection):
 
     async def send_message(self, message, address):
         message_encode = self.encode(message)
         self.server.sendto(message_encode, address)
     
     def run_send(self):
+        contacts = Contacts()
         while True:
+            name = input("contact: ")
             message = input("mesage: ")
-            address = ("192.168.50.10", 6001)
-            asyncio.run(self.send_message(message, address))
+            contact = (contacts.search_contact(name), 5024)
+            asyncio.run(self.send_message(message, contact))
 
 if __name__ == "__main__":
-    send = multiprocessing.Process(target=Send_Message().run_send()).start()
+    port = 5026
+    buffer = 1024
+    send = multiprocessing.Process(target=Send_Message(port, buffer).run_send()).start()
--- server.py
+++ server.py
@@ -21,3 +21,19 @@ class Connection(ABC):
     
     def decode(self, message):
         return message.decode('UTF-8')
+
+class Reciv_Message(Connection):
+
+    async def reciv_message(self):
+        data_encode, address = self.server.recvfrom(self.buffer)
+        data = self.decode(data_encode)
+        return data, address
+    
+    def run_recv(self):
+        while (True):
+            print("...")
+            data, address = asyncio.run(self.reciv_message())
+            print(f"message: {data=} send to for {address=}")
+
+if __name__ ==  "__main__":
+    recv = multiprocessing.Process(target=Reciv_Message().run_recv()).start()
